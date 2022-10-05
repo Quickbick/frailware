c79c3f6fe679f0997fe200232b54858188853bbc
Nathanael Ostheller <nathanael.ostheller@wsu.edu>
2022-10-05 15:52:48 -0700

    Switched to physics based movement system

--- Assets/protagControls.cs
+++ Assets/protagControls.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 public class protagControls : MonoBehaviour
 {
     public Vector2 speed = new Vector2(50, 50);
+    public Rigidbody2D rb;
+    public float jumpHeight = 10;
 
     // Update is called once per frame
     void Update()
     {
+        //allows left and right movement
         float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
-
+        Vector3 movement = new Vector3(speed.x * inputX, 0, 0);
         movement *= Time.deltaTime;
-
         transform.Translate(movement);
     }
 }
