aafc694e695004c7f0547a6584ae513970bb2222
Nate Ostheller <nsostheller@gmail.com>
2022-11-23 22:04:49 -0800

    started corperate gameworld

---
+++ Assets/Scripts/ground.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ground : MonoBehaviour, IGround
+{
+    // Start is called before the first frame update
+    void IGround.Stepped(){
+        
+    }
+}
